2579aaba59e386484091bddd4c8b50e41eca6035
Victor Hugo Campos Belo <victorhugocamposbelo@gmail.com>
2022-07-24 00:03:05 -0300

    Ex 002

--- Exercicios/README.md
+++ Exercicios/README.md
@@ -2,3 +2,4 @@
 ## Aula 001
 Mostra na tela Ola, mundo
 ## Aula 002
+Mensagem de bem-vindo
---
+++ Exercicios/ex002.py
@@ -0,0 +1,3 @@
+name = input("Qual seu nome: ")
+
+print(f"E um prazer te conhecer, {str(name).capitalize()}!")
